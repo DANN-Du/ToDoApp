b700e5ae5bc6c362342a722b7c6e9f9506c88bd5
LENOVO <danielduquegomez@gmail.com>
2024-08-26 13:51:13 -0500

    eighth point: pending_todos method was created

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -30,6 +30,13 @@ class TodoBook:
         self.todos [id] = new_object
         return id
 
+    def pending_todos(self) -> list:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+
+
+
+
 
 
 
